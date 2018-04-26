26aeaa8eb96c3e123955c6246401e4785426f09e
jaxquelina <37966116+jaxquelina@users.noreply.github.com>
2018-04-26 13:53:23 -0300

    Add files via upload

---
+++ Trabajo practico 2/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "estructuras.h"
+
+#define CANT 5
+
+int main()
+{
+    struct Epersona gente[CANT];
+    int salir=0;
+    int gente18 , genteDel9a35 ,gentemayorde35;
+
+    inicializarPersonas(gente, CANT);
+
+    do
+    {
+        switch(menu())
+        {
+
+        case 1:
+            altaPersona(gente,CANT);
+            system("pause");
+            break;
+
+
+        case 2:
+            bajaPersona(gente,CANT);
+            system("pause");
+            break;
+
+
+        case 3:
+            ordenarPersonas(gente,CANT);
+            system("pause");
+            break;
+
+
+        case 4:
+           gente18=0;
+           genteDel9a35=0;
+           gentemayorde35=0;
+           graficoPersonas(gente, gente18, genteDel9a35, gentemayorde35, CANT);
+           system("pause");
+
+            break;
+
+        case 5:
+            salir = 1;
+            break;
+
+
+        default:
+            printf("\nOpcion Incorrecta\n\n");
+            system("pause");
+           }
+      }while(salir != 1);
+
+return 0;
+}
